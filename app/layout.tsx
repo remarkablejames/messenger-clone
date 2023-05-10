820619b66dc78a1c8da0633d6ce14b83711c919e
James Niyongira <lascasasjames85@gmail.com>
2023-05-10 09:43:03 -0400

    finished developing application

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,27 @@
-import "./globals.css";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import './globals.css'
+import AuthContext from './context/AuthContext'
+import ActiveStatus from './components/ActiveStatus'
+import ToasterContext from './context/ToasterContext'
 
 export const metadata = {
-  title: "Messenger",
-  description:
-    "Messenger clone built with Next.js and Tailwind CSS  by James Niyongira",
-};
+  title: 'Messenger',
+  description: 'Messenger Clone',
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <AuthContext>
+          <ToasterContext />
+          <ActiveStatus />
+          {children}
+        </AuthContext>
+      </body>
     </html>
-  );
+  )
 }
